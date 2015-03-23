128944f0dc30bde006a2faa5e4c23a916f89de87
andy fang <andy@mingtak.com.tw>
2015-03-23 14:34:24 +0000

    initial version

---
+++ funlog/portlet/googlead/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+GoogleAdMessageFactory = MessageFactory('funlog.portlet.googlead')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
